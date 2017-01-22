25a9e76f03b83818f88d4d19412ec9c9e573364c
sudheer52 <varma4147@gmail.com>
2017-01-22 15:11:42 +0530

    changes to package

--- Installpackages.pp
+++ Installpackages.pp
@@ -1,15 +1,16 @@
 # To install Apache and Tomcat package 
 
-
 $package_install = $facts['os']['family']? {
-    'Redhat' => 'httpd' , 
-     'Debian' => 'apache2' ,         
+    'Redhat' => 'httpd' ,  'tomcat',
+     'Debian' => 'apache2' , 'tomcat7' ,        
 }
 
+#Ensure package is installed 
 package { $package_install :
     ensure => installed,
 }
 
+#ENsure package is running
 service { $package_install:
     ensure     => running,
     enable     => true,
